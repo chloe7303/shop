5683194bb4d789cbcb30cac4e17245ada9230f74
chloe7303 <u10517004@go.utaipei.edu.tw>
2020-11-10 13:32:45 +0800

    uplaod v1

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,47 @@
-<template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+<template lang="pug">
+  #home
+    b-row
+      b-col(cols="12" md="6" lg="3" v-for="(item, index) in items" :key="index")
+        b-card(:title="item.name" :img-src="item.img" img-top :sub-title="'$'+item.price")
+          b-button(variant="primary" @click="addCart(item)")
+            font-awesome-icon(:icon="['fas', 'shopping-cart']")
+            | &nbsp; 加入購物車
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
   name: 'Home',
-  components: {
-    HelloWorld
+  data () {
+    return {
+      items: [
+        {
+          img: 'https://c.ecimg.tw/items/DCAN3XA9009CGDK/000002_1535018120.jpg',
+          name: 'E-books MP2 無印風極簡滑鼠墊',
+          price: 129
+        },
+        {
+          img: 'https://b.ecimg.tw/items/DHAFIRA900AEYQR/000002_1596715202.jpg',
+          name: 'ASUS VivoBook 玫瑰金',
+          price: 23900
+        },
+        {
+          img: 'https://f.ecimg.tw/items/DCAN8JA900A5XTF/000002_1586943602.jpg',
+          name: '羅技 鵝卵石無線滑鼠-玫瑰粉',
+          price: 699
+        },
+        {
+          img: 'https://d.ecimg.tw/items/DCAHBCA9009Y2B1/000002_1586943602.jpg',
+          name: '羅技 跨平台藍牙鍵盤-玫瑰粉',
+          price: 990
+        }
+      ]
+    }
+  },
+  methods: {
+    addCart (item) {
+      // 呼叫 mutations
+      this.$store.commit('addCart', item)
+    }
   }
 }
 </script>
